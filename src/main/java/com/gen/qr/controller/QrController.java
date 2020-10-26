257713b8eacac5d456fe9931034f1afba6acf0c5
dimitar <dimitur.chikalanov@gmail.com>
2020-10-26 11:45:51 +0200

    qr

--- src/main/java/com/gen/qr/controller/QrController.java
+++ src/main/java/com/gen/qr/controller/QrController.java
@@ -2,10 +2,11 @@ package com.gen.qr.controller;
 
 
 import com.gen.qr.service.QrService;
-import org.springframework.http.HttpStatus;;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.image.BufferedImage;
 
